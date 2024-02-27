fcb0e9dfcb02c5f8b645712c2bde287603227ec5
Bryan <bryanagan25@gmail.com>
2024-02-27 10:34:29 +0800

    Add FrameHolder.java, a class that manages the frame of the app

---
+++ src/FrameHolder.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class FrameHolder extends JFrame{
+
+    FrameHolder(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Student Attendance Application by xbryan25");
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.setVisible(true);
+
+    }
+}

7a0963895917a3acbd9483258067824e09073bde
Bryan <bryanagan25@gmail.com>
2024-02-27 21:55:53 +0800

    Started learning on how to use GridBagLayout

--- src/FrameHolder.java
+++ src/FrameHolder.java
@@ -2,25 +2,27 @@ import javax.swing.JFrame;
 
 public class FrameHolder extends JFrame{
     IntroScreen introScreen;
-    AttendanceScreen attendanceScreen;
+    AttendanceScreen attendanceScreen = new AttendanceScreen();
+
     FrameHolder(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Student Attendance Application by xbryan25");
         this.setSize(500, 500);
+        this.setLocationRelativeTo(null);
         this.setLayout(null);
         this.setVisible(true);
         this.setResizable(false);
 
         introScreen = new IntroScreen(this);
         this.add(introScreen);
+        this.add(attendanceScreen);
 
+        attendanceScreen.setVisible(false);
 
     }
 
     public void changeToAttendanceScreen(){
-
-        attendanceScreen = new AttendanceScreen();
-        this.add(attendanceScreen);
+        introScreen.setVisible(false);
         attendanceScreen.setVisible(true);
     }
 }
--- src/IntroScreen.java
+++ src/IntroScreen.java
@@ -1,41 +1,65 @@
 // TODO: Buttons stay at the top, use a Layout Manager
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
 
 public class IntroScreen extends JPanel implements ActionListener{
     JButton addStudentButton = new JButton("Add Student");
+
     JButton aboutThisAppButton = new JButton("About this app");
     JButton adminModeButton = new JButton("Go Admin?");
 
     FrameHolder frame;
+    GridBagConstraints gbc = new GridBagConstraints();
+
+
     IntroScreen(FrameHolder frame){
         this.frame = frame;
-
+        this.setLayout(new GridBagLayout());
+        this.setSize(500, 500);
         this.setBackground(Color.RED);
-        this.setBounds(0, 0, 500, 500);
 
-        addStudentButton.setBounds(50, 150, 150, 50);
+        gbc.insets = new Insets(0, 3, 3, 3);
+
+        addStudentButton.setPreferredSize(new Dimension(180, 50));
+        addStudentButton.setFont(new Font("Arial", Font.BOLD, 20));
         addStudentButton.addActionListener(this);
 
-        adminModeButton.setBounds(250, 150, 150, 50);
-        adminModeButton.addActionListener(this);
-
-        aboutThisAppButton.setBounds(250, 350, 150, 50);
+        aboutThisAppButton.setPreferredSize(new Dimension(180, 50));
+        aboutThisAppButton.setFont(new Font("Arial", Font.BOLD, 20));
         aboutThisAppButton.addActionListener(this);
 
-        this.add(addStudentButton);
-        this.add(aboutThisAppButton);
-        this.add(adminModeButton);
+        adminModeButton.addActionListener(this);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridheight = 1;
+        this.add(addStudentButton, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridheight = 1;
+        this.add(aboutThisAppButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.insets = new Insets(50, 0, 0, 0);
+        this.add(adminModeButton, gbc);
+
+
+//        gbc.gridx = 3;
+//        gbc.gridy = 3;
+//        gbc.weightx = 1;
+//        gbc.weighty = 1;
+//
+//        this.add(new JLabel(" "), gbc);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addStudentButton) {
-            this.hidePanel();
             this.frame.changeToAttendanceScreen();
         } else if (e.getSource() == aboutThisAppButton){
             System.out.println("aboutThisAppButton");
@@ -43,12 +67,4 @@ public class IntroScreen extends JPanel implements ActionListener{
             System.out.println("adminModeButton");
         }
     }
-
-    public void hidePanel(){
-        this.setVisible(false);
-    }
-
-//    public void showPanel(){
-//        this.setVisible(true);
-//    }
 }

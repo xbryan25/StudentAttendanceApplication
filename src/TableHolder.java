6c642c50789b652fec25f84804911e53f8e96f73
Bryan <bryanagan25@gmail.com>
2024-03-06 21:28:42 +0800

    Add table (no rows and columns yet, only headers)

--- src/TableHolder.java
+++ src/TableHolder.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TableHolder extends JPanel{
-
+    AttendanceTable table = new AttendanceTable();
     TableHolder(){
         this.setBackground(Color.ORANGE);
         this.setBounds(0, 100, 500, 400);
+
+        this.add(table.pane);
     }
 }

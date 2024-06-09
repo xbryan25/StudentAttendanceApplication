3129a266d2499e786cbff4b56d9bef4b27763573
Bryan <bryanagan25@gmail.com>
2024-06-09 21:21:39 +0800

    Stop event title names that only contain whitespaces in 'Rename Event'; stop renaming to null

--- src/main/AdminScreen.java
+++ src/main/AdminScreen.java
@@ -425,7 +425,7 @@ public class AdminScreen extends JPanel implements ActionListener{
                 eventTitle = JOptionPane.showInputDialog(null, "To proceed, input a name for the event.", "",
                         JOptionPane.QUESTION_MESSAGE);
 
-                if (eventTitle != null && !eventTitle.isEmpty() && eventTitle.length() <= 20) {
+                if (eventTitle != null && !eventTitle.isEmpty() && !eventTitle.isBlank() && eventTitle.length() <= 20) {
                     break;
                 } else if (eventTitle == null) {
                     eventTitleCancel = true;
@@ -438,10 +438,13 @@ public class AdminScreen extends JPanel implements ActionListener{
                             "", JOptionPane.WARNING_MESSAGE);
                 }
             }
-            this.tableHolder.setTitle(eventTitle);
 
-            JOptionPane.showMessageDialog(null, "Event successfully renamed to " + eventTitle + ".",
-                    "", JOptionPane.INFORMATION_MESSAGE);
+            if (eventTitle != null){
+                this.tableHolder.setTitle(eventTitle);
+
+                JOptionPane.showMessageDialog(null, "Event successfully renamed to " + eventTitle + ".",
+                        "", JOptionPane.INFORMATION_MESSAGE);
+            }
         }
         else if(e.getSource() == saveProgress){
             if (!frame.tableHasData){
